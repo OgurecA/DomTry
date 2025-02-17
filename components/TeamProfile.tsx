3d7f76b1d52cdab1701a0d1fd6c75b7ce7d7429b
OgurecA <acpwork044@gmail.com>
2025-02-17 17:28:47 +0400

    k

--- components/TeamProfile.tsx
+++ components/TeamProfile.tsx
@@ -4,12 +4,18 @@ import styles from "../styles/TeamProfile.module.css";
 const TeamProfile = ({ name, score, className, bestPlayer }) => {
   return (
     <div className={className}>
-      <h2 className={styles.teamName}>{name}</h2>
-      <p className={styles.teamScore}>Очки: {score}</p>
-      <p className={styles.teamAlpha}
-        onClick={() => window.open(`https://explorer.solana.com/address/${bestPlayer}?cluster=devnet`, "_blank")}>
-          Alpha Predator: {bestPlayer.slice(0, 4)}...{bestPlayer.slice(-4)}
-      </p>
+      <div className={styles.teamContent}>
+        <h2 className={styles.teamName}>{name}</h2>
+        <p className={styles.teamScore}>Очки: {score}</p>
+        <p
+          className={styles.teamAlpha}
+          onClick={() =>
+            window.open(`https://explorer.solana.com/address/${bestPlayer}?cluster=devnet`, "_blank")
+          }
+        >
+          Alpha Predator: {bestPlayer?.slice(0, 4)}...{bestPlayer?.slice(-4)}
+        </p>
+      </div>
     </div>
   );
 };
