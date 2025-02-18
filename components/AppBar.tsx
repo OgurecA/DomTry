4239aaea611a91b96e73b79b9b2c2cf25e5a1371
OgurecA <acpwork044@gmail.com>
2025-02-18 12:16:44 +0400

    k

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -28,7 +28,7 @@ export const AppBar: FC = () => {
                 {/* <button className={styles.NavButton} onClick={() => navigateTo('/office')}>OFFICE</button> */}
             </div>
             <div className={styles.RightSection}>
-                <WalletMultiButton className={styles.walletButton}/>
+                <WalletMultiButton className={styles.walletButton}/>{/* НЕ РАБОТАЕТ ИЗ-ЗА СТИЛЕЙ */}
             </div>
         </div>
     );
