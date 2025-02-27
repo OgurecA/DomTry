0a4ac38b18ff9350eb140246b25a4d031304fdfa
OgurecA <acpwork044@gmail.com>
2025-02-27 15:25:15 +0400

    k

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,9 +1,23 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export const AppBar: FC = () => {
-          
+    
+    const [isMobileLayout, setIsMobileLayout] = useState<boolean | null>(false);
+    
+        useEffect(() => {
+            const handleResize = () => {
+                setIsMobileLayout(window.innerWidth < 780);
+            };
+    
+            // Вызываем сразу при загрузке
+            handleResize();
+            window.addEventListener("resize", handleResize);
+    
+            return () => window.removeEventListener("resize", handleResize);
+        }, []);
+        
     return (
         <div className={styles.AppHeader}>
             <div className={styles.LeftSection}>
@@ -12,7 +26,9 @@ export const AppBar: FC = () => {
                 {/* <button className={styles.NavButton} onClick={() => navigateTo('/office')}>OFFICE</button> */}
             </div>
             <div className={styles.RightSection}>
-                <WalletMultiButton className={styles.walletButton}/> {/* НЕ РАБОТАЕТ ИЗ-ЗА СТИЛЕЙ */}
+            {!isMobileLayout && (
+                <WalletMultiButton className={styles.walletButton}/>
+            )} {/* НЕ РАБОТАЕТ ИЗ-ЗА СТИЛЕЙ */}
             </div>
         </div>
     );
