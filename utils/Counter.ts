826fe6711a7f30714a476d56a3963990c477ffc7
OgurecA <acpwork044@gmail.com>
2025-03-13 18:00:47 +0400

    k

--- utils/Counter.ts
+++ utils/Counter.ts
@@ -184,8 +184,8 @@ const updateTeamPoints = async () => {
 const db = new sqlite3.Database("game.db");
 
 // ⚡ Задаем время выполнения (в UTC)
-const EXECUTION_HOUR = 13;  // Часы (от 0 до 23)
-const EXECUTION_MINUTE = 57; // Минуты (от 0 до 59)
+const EXECUTION_HOUR = 0;  // Часы (от 0 до 23)
+const EXECUTION_MINUTE = 0; // Минуты (от 0 до 59)
 
 
 // Функция, которая будет выполняться в заданное время
